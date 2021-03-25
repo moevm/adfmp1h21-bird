ec69b2b03faf13e148b063eb1eff1887e3fa014b
user00 <dihindee@gmail.com>
2021-03-25 21:53:00 +0300

    Добавлена активность входа

---
+++ adfmp1h21bird/app/src/main/java/com/example/adfmp1h21_bird/ui/login/LoggedInUserView.kt
@@ -0,0 +1,9 @@
+package com.example.adfmp1h21_bird.ui.login
+
+/**
+ * User details post authentication that is exposed to the UI
+ */
+data class LoggedInUserView(
+        val displayName: String
+        //... other data fields that may be accessible to the UI
+)
---
+++ adfmp1h21bird/app/src/main/java/com/example/adfmp1h21_bird/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package com.example.adfmp1h21_bird.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(val usernameError: Int? = null,
+                          val passwordError: Int? = null,
+                          val isDataValid: Boolean = false)
