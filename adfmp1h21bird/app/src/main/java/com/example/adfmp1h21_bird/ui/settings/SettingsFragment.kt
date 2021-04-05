b55c1dc4d2446d10852418a79222b679be79e534
Sergey <serezha871@gmail.com>
2021-04-05 12:08:42 +0300

    Макет настроек. На кновки и переключатель повешены тосты

--- adfmp1h21bird/app/src/main/java/com/example/adfmp1h21_bird/ui/settings/SettingsFragment.kt
+++ adfmp1h21bird/app/src/main/java/com/example/adfmp1h21_bird/ui/settings/SettingsFragment.kt
@@ -6,11 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CompoundButton
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.SwitchCompat
 import androidx.lifecycle.Observer
 import com.example.adfmp1h21_bird.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : Fragment(), View.OnClickListener,
+    CompoundButton.OnCheckedChangeListener {
 
 //    companion object {
 //        fun newInstance() = SettingsFragment()
@@ -21,14 +26,23 @@ class SettingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val rootView:View = inflater.inflate(R.layout.fragment_settings, container, false)
         viewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
 
-        val textView: TextView = rootView.findViewById(R.id.setting_text)
-        viewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        val textView: TextView = rootView.findViewById(R.id.setting_text)
+//        viewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+
+        val clear_btn:Button = rootView.findViewById(R.id.settings_btn_clear_statistics)
+        clear_btn.setOnClickListener(this)
+
+        val save_btn:Button = rootView.findViewById(R.id.settings_btn_save)
+        save_btn.setOnClickListener(this)
+
+        val switch_geotag: SwitchCompat = rootView.findViewById(R.id.settings_switch_geotag)
+        switch_geotag.setOnCheckedChangeListener(this)
 
         return rootView
     }
@@ -39,4 +53,36 @@ class SettingsFragment : Fragment() {
 ////         TODO: Use the ViewModel
 //    }
 
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when(v.id){
+                R.id.settings_btn_clear_statistics ->{
+                    Toast.makeText(context, "Clear statistics!",Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.settings_btn_save ->{
+                    Toast.makeText(context, "Save settings!",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
+
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+        if (buttonView != null) {
+            when(buttonView.id){
+                R.id.settings_switch_geotag ->{
+                    if(isChecked){
+                        Toast.makeText(context, "Auto geotag ON!",Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(context, "Auto geotag OFF!",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+
 }
+
